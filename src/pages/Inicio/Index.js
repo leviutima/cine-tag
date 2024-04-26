f70ac47dde4245c096e083f4e596dc36484af2e1
Levi_Utima <146796839+leviutima@users.noreply.github.com>
2024-04-26 11:24:38 -0300

    adicionando card json

--- src/pages/Inicio/Index.js
+++ src/pages/Inicio/Index.js
@@ -1,12 +1,24 @@
+import styles from './Inicio.module.css'
 import Banner from "components/Banner/Index"
 import Cabecalho from "components/Cabecalho/Index"
+import Card from "components/Card/Index"
 import Rodape from "components/Rodape/Index"
+import Titulo from "components/Titulo/Index"
+import videos from 'json/db.json'
 
 function Inicio () {
     return(
         <>
             <Cabecalho/>
             <Banner imagem='Home'/>
+            <Titulo>
+                <h1>Assista aos melhores vídeos virais!</h1>
+            </Titulo>
+            <section className={styles.container}>
+                {videos.map((video ) => {
+                   return <Card {...video} key={video.id}/>
+                })}
+            </section>
             <Rodape />
         </>
     )
